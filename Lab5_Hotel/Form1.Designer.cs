3d8fe02e4229c64ee8e6090da169a79381601829
LadyVamp <ladyvamp666@gmail.com>
2017-05-03 21:15:39 +0300

    Init

---
+++ Lab5_Hotel/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Lab5_Hotel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtFio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.cmbOfhotel = new System.Windows.Forms.ComboBox();
+            this.clientBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lR5DataSet = new Lab5_Hotel.LR5DataSet();
+            this.clientTableAdapter = new Lab5_Hotel.LR5DataSetTableAdapters.ClientTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lR5DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(26, 206);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(245, 9);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(502, 220);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // txtFio
+            // 
+            this.txtFio.Location = new System.Drawing.Point(112, 14);
+            this.txtFio.Name = "txtFio";
+            this.txtFio.Size = new System.Drawing.Size(100, 20);
+            this.txtFio.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ФИО";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Номер";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(112, 51);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Гражданство";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(112, 130);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(100, 20);
+            this.txtCountry.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Возраст";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(112, 93);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 167);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Отель";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(112, 206);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(26, 245);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // cmbOfhotel
+            // 
+            this.cmbOfhotel.DataSource = this.clientBindingSource;
+            this.cmbOfhotel.DisplayMember = "ofhotel";
+            this.cmbOfhotel.FormattingEnabled = true;
+            this.cmbOfhotel.Location = new System.Drawing.Point(112, 164);
+            this.cmbOfhotel.Name = "cmbOfhotel";
+            this.cmbOfhotel.Size = new System.Drawing.Size(100, 21);
+            this.cmbOfhotel.TabIndex = 16;
+            this.cmbOfhotel.ValueMember = "ofhotel";
+            // 
+            // clientBindingSource
+            // 
+            this.clientBindingSource.DataMember = "Client";
+            this.clientBindingSource.DataSource = this.lR5DataSet;
+            // 
+            // lR5DataSet
+            // 
+            this.lR5DataSet.DataSetName = "LR5DataSet";
+            this.lR5DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // clientTableAdapter
+            // 
+            this.clientTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Испанские отели 4 или 5 *";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Клиенты греческих отелей";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(245, 307);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Отели с двумя и более клиентами";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(245, 349);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(264, 23);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Клиенты, живущие в отелях, где нет французов";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 398);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbOfhotel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFio);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnInsert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lR5DataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtFio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ComboBox cmbOfhotel;
+        private LR5DataSet lR5DataSet;
+        private System.Windows.Forms.BindingSource clientBindingSource;
+        private LR5DataSetTableAdapters.ClientTableAdapter clientTableAdapter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Lab5_Hotel/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab5_Hotel
+{
+    public partial class Form1 : Form
+    {
+        public DataClasses1DataContext db;
+        SqlConnection con = new SqlConnection("Data Source=NADYA-PC;Initial Catalog=LR5;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+     
+        
+        int ID = 0;
+        int Num = 0;
+        int Age = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new DataClasses1DataContext();
+            DisplayData();
+        }
+        //  CRUD
+        //Insert 
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (txtFio.Text != "" && txtNum.Text != "")
+            {
+                cmd = new SqlCommand("insert into Client(fio, num, age, country, ofhotel) values(@fio, @num, @age, @country, @ofhotel)", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@fio", txtFio.Text);
+                cmd.Parameters.AddWithValue("@num", txtNum.Text);
+                cmd.Parameters.AddWithValue("@age", txtAge.Text);
+                cmd.Parameters.AddWithValue("@country", txtCountry.Text);
+                cmd.Parameters.AddWithValue("@ofhotel", cmbOfhotel.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record successfully inserted!");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Fill in all the fields");
+            }
+        }
+
+        //Update
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtFio.Text != "" && txtNum.Text != "")
+            {
+                cmd = new SqlCommand("update Client set Fio=@fio, Num=@num, Age=@age, Country=@country, Ofhotel=@ofhotel where ID=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", ID);
+                cmd.Parameters.AddWithValue("@fio", txtFio.Text);
+                cmd.Parameters.AddWithValue("@num", txtNum.Text);
+                cmd.Parameters.AddWithValue("@age", txtAge.Text);
+                cmd.Parameters.AddWithValue("@country", txtCountry.Text);
+                cmd.Parameters.AddWithValue("@ofhotel", cmbOfhotel.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record successfully updated!");
+                con.Close();
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please select record to Update");
+            }
+        }
+        //Delete
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (ID != 0)
+            {
+                cmd = new SqlCommand("delete Client where ID=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", ID);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record successfully deleted!");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please select record to Delete");
+            }
+        }
+
+
+        //Display Data in DataGridView  
+        private void DisplayData()
+        {
+            con.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("select * from Client", con);
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+        //Clear Data  
+        private void ClearData()
+        {
+            txtFio.Text = "";
+            txtNum.Text = "";
+            txtAge.Text = "";
+            txtCountry.Text = "";
+            cmbOfhotel.Text = "";
+            ID = 0;
+        }
+        //  /CRUD
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e) 
+        {
+            txtFio.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            Num = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString());
+            Age = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString());
+            txtCountry.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            cmbOfhotel.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString());
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "lR5DataSet.Client". При необходимости она может быть перемещена или удалена.
+            this.clientTableAdapter.Fill(this.lR5DataSet.Client);
+
+        }
+
+        //  Queries with filter
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            IQueryable<Hotels> spainhotels = from c in db.Hotels
+                                             where c.hcountry == "Spain" && (c.stars == 4 || c.stars == 5)
+                                         select c;
+            string s = "";
+            foreach (Hotels hotels in spainhotels)
+            {
+                s += "\nГород: " + hotels.hcity + ", Название: " + hotels.hname  + ", Кол-во звезд: " + hotels.stars;
+
+            }
+            MessageBox.Show("Отели Испании 4 или 5 звезд: \n" + s);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+ 
+
+
+
+    }
+}
